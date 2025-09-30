43ac413d55f67eb3e655c90669fff63331a66305
SpeTIX <christian@cassano.fe.it>
2025-09-30 22:48:38 +0000

    add doc into exported interfaces

--- pkg/barout/blocketOutputInterface.go
+++ pkg/barout/blocketOutputInterface.go
@@ -7,10 +7,17 @@ import (
 	"github.com/spetix/bar-out-adapters/pkg/barout/data"
 )
 
+
+// BlockletOutput is the interface that wraps the Print method. It is used to output data in different formats. 
+// An implementation of this interface is provided for each supported protocol.
+
 type BlockletOutput interface {
 	Print(d data.Data)
 }
 
+
+// New creates a new BlockletOutput based on the provided protocol string.
+// It supports "json", "raw", and "waybar" protocols. If an unsupported protocol is provided, it defaults to "raw".
 func New(protocol string) BlockletOutput {
 
 	switch protocol {
--- pkg/barout/data/dataInterface.go
+++ pkg/barout/data/dataInterface.go
@@ -1,5 +1,8 @@
 package data
 
+
+// Data is the interface that wraps methods to retrieve information about a blocklet's output.
+// Client code can implement this interface to provide custom data for different blocklets.
 type Data interface {
 	Short() string
 	Long() string
