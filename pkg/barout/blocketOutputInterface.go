1c11ae319fc18c10539726a7d882f86f3eecfdaf
SpeTIX <christian@cassano.fe.it>
2026-06-14 01:37:50 +0200

    added common command flags

--- pkg/barout/blocketOutputInterface.go
+++ pkg/barout/blocketOutputInterface.go
@@ -7,26 +7,52 @@ import (
 	"github.com/spetix/bar-out-adapters/pkg/barout/data"
 )
 
-
-// BlockletOutput is the interface that wraps the Print method. It is used to output data in different formats. 
+// BlockletOutput is the interface that wraps the Print method. It is used to output data in different formats.
 // An implementation of this interface is provided for each supported protocol.
 
 type BlockletOutput interface {
 	Print(d data.Data)
 }
 
+type BlockletProtocol string
+
+const (
+	Raw      BlockletProtocol = "raw"
+	I3Blocks BlockletProtocol = "i3blocks"
+	Waybar   BlockletProtocol = "waybar"
+	Json     BlockletProtocol = "json"
+)
+
+func (p BlockletProtocol) String() string {
+	return string(p)
+}
+
+func ParseProtocol(protoStr string) BlockletProtocol {
+	switch protoStr {
+	case "raw":
+		return Raw
+	case "i3blocks":
+		return I3Blocks
+	case "waybar":
+		return Waybar
+	case "json":
+		return Json
+	default:
+		return Raw
+	}
+}
 
 // New creates a new BlockletOutput based on the provided protocol string.
 // It supports "json", "raw", and "waybar" protocols. If an unsupported protocol is provided, it defaults to "raw".
-func New(protocol string) BlockletOutput {
+func New(protocol BlockletProtocol) BlockletOutput {
 
 	switch protocol {
-	case "json":
-		return protocols.NewJsonOut(os.Stdout)
-	case "raw":
+	case Raw, I3Blocks:
 		return protocols.NewRawOut(os.Stdout)
-	case "waybar":
+	case Waybar:
 		return protocols.NewWaybarOut(os.Stdout)
+	case Json:
+		return protocols.NewJsonOut(os.Stdout)
 	default:
 		return protocols.NewRawOut(os.Stdout)
 	}
